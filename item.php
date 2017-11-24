59ccc4a056b90ad76ca6c0146fbfadac715317a9
ahtec <gerarddoets@gmail.com>
2017-11-24 14:29:57 +0100

    nu met json

---
+++ item.php
@@ -0,0 +1,30 @@
+<?php
+class item{
+        public $item        ;
+        public $description ;
+        public $stock       ;
+        public $minstock    ;
+        public $maxstock    ;
+        public $warehouse   ;
+//        
+//function __construct(
+//            $p_item        ,       
+//            $p_description ,         
+//            $p_stock       ,       
+//            $p_minstock    ,        
+//            $p_maxstock    ,
+//            $p_warehouse   ) 
+//        
+//        {
+//    
+//   $item        = $p_item          ;
+//   $description = $p_description   ; 
+//   $stock       = $p_stock         ;
+//   $minstock    = $p_minstock      ; 
+//   $maxstock    = $p_maxstock      ;
+//   $warehouse   = $p_warehouse     ;
+//    
+//    
+//        }
+        
+}
