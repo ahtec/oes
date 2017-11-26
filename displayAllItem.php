05c6ee61f73cae7325d1423e4b2520e8ea9b6f65
ahtec <gerarddoets@gmail.com>
2017-11-26 18:30:59 +0100

    voorlopig klaar met items

---
+++ displayAllItem.php
@@ -0,0 +1,58 @@
+<html>
+    <head>
+        <title>Order Entry Sytem</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <link rel = "stylesheet" type = "text/css" href="oesCss.css"> 
+         
+         
+           <script>
+        function cansel() {
+//                toCansel = true;
+                window.location.assign("itemMenu.html");
+//               alert("In de cansel")
+            }
+
+        </script>
+
+    </head>
+    <body>
+
+<?php
+session_start();
+require_once './connection.php';
+$returnText = "";
+
+$conn = connectToDb();
+
+if (!$conn->connect_error) {
+    $sql="SELECT * FROM `item`";
+    $result = $conn->query($sql);
+    echo "<table>
+            <tr>
+                <th>Item</th>
+                <th>Descriptionww</th>
+                <th>Stock</th>
+                <th>Minnimum Stock</th>
+                <th>Maximum Stock</th>
+                <th>Warehouse</th>
+            </tr>";
+    while ($row = mysqli_fetch_array($result)) {
+        echo "<tr>";
+        echo "<td>" . $row['item'] . "</td>";
+        echo "<td>" . $row['description'] . "</td>";
+        echo "<td>" . $row['stock'] . "</td>";
+        echo "<td>" . $row['minStock'] . "</td>";
+        echo "<td>" . $row['maxStock'] . "</td>";
+        echo "<td>" . $row['warehouse'] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+
+    mysqli_close($conn);
+}
+    ?>
+
+
+<button onclick="cansel()" >Ready</button>
+</body>
