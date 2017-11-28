b79ffc24de9811cf23e1abb527eea92b7ecb9594
ahtec <gerarddoets@gmail.com>
2017-11-28 14:46:45 +0100

    probleem niet goed gedelete orderline

--- insertOrderLines.php
+++ insertOrderLines.php
@@ -7,6 +7,12 @@ $delDate = $_SESSION['delDate'];
 $customer = $_SESSION['customer'];
 require_once './connection.php';
 
+
+if (isset($_REQUEST)) {
+    if (isset($_REQUEST['errorText'])) {
+        echo $_REQUEST['errorText'];
+    }
+}
 ?>
 
 <html>
@@ -23,7 +29,7 @@ require_once './connection.php';
     <body>
         <table>
             <form name="displayOrder" action="insertOrder.php"    method =POST>
-                <?php
+<?php
 echo <<<MYTAG
                         <tr> <td> order             </td> <td> $order    </td></tr>
                         <tr> <td> order description </td> <td> $desc     </td></tr>
@@ -31,83 +37,82 @@ echo <<<MYTAG
                         <tr> <td> delivery date     </td> <td> $delDate  </td></tr>
                         <tr> <td> customer          </td> <td> $customer </td></tr>
 MYTAG;
-                ?>
+?>
                 </td></tr>
             <!--<tr> <td> hours to get this <br>item delivered to the <br>warehouse</td> <td><input type="text" name="delTime" value="10" size="30" /></td></tr>-->
                 <tr> <td>  <br><br>   <input type="submit" value="OK" id=screenButtons></td></tr>
         </table>  
     </form>
 
-    <?php
+<?php
 //lees orderlines 
-    $conn = connectToDb();
-    if (!$conn->connect_error) {
-        $sql = sprintf("SELECT * FROM  `orderLines`  where  `order` = %d", $order);
-        echo $sql;
-        $result = mysqli_query($conn, $sql);
+$conn = connectToDb();
+if (!$conn->connect_error) {
+    $sql = sprintf("SELECT * FROM  `orderLines`  where  `order` = %d", $order);
+//    echo $sql;
+    $result = mysqli_query($conn, $sql);
 
-        $sql = sprintf("SELECT * FROM  `item` ");
-        echo $sql;
-        $itemResult = mysqli_query($conn, $sql);
+    $sql = sprintf("SELECT * FROM  `item` ");
+//    echo $sql;
+    $itemResult = mysqli_query($conn, $sql);
+}
 
-    }
+echo "<table>";
+while ($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['order'] . "</td>";
+    echo "<td>" . $row['item'] . "</td>";
+    echo "<td>" . $row['amount'] . "</td>";
+    echo "<td>" . $row['lineText'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
 
-    echo "<table>";
-    while ($row = mysqli_fetch_array($result)) {
-        echo "<tr>";
-        echo "<td>" . $row['order'] . "</td>";
-        echo "<td>" . $row['item'] . "</td>";
-        echo "<td>" . $row['amount'] . "</td>";
-        echo "<td>" . $row['lineText'] . "</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
+echo "<form name=insertOrderLinesDB   action=insertOrderLinesDB.php   method =GET>";
 
-    echo "<form name=insertOrderLinesDB   action=insertOrderLinesDB.php   method =GET>";
+echo "<table>";
+$i = 0;
+while ($row = mysqli_fetch_array($itemResult)) {
+    $aantal = geefAnntalVanItemInOrderLines($order, $row['item']);
+    $i++;
+    echo "<tr>";
+    echo "<td>" . $row['item'] . "</td><td> <input type=text name=aantal" . $i . "  id=IDnaam  value =$aantal  > </td>";
+    echo "<td>" . $row['item'] . "</td><td> <input type=hidden name=hiddenaantal" . $i . "  id=IDnaam  value =$aantal   > </td>";
+    echo "<td>" . $row['description'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+echo " <tr> <td>   </td> <td>    <input type=submit value=Ready >   </td> </tr>";
 
-    echo "<table>";
-    $i = 0;
-    while ($row = mysqli_fetch_array($itemResult)) {
-        $aantal = geefAnntalVanItemInOrderLines($order, $row['item']);
-        $i++;
-        echo "<tr>";
-        echo "<td>" . $row['item'] . "</td><td> <input type=text name=aantal".$i."  id=IDnaam  value =$aantal  > </td>";
-        echo "<td>" . $row['item'] . "</td><td> <input type=hidden name=hiddenaantal".$i."  id=IDnaam  value =$aantal   > </td>";
-        echo "<td>" . $row['description'] . "</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-    echo " <tr> <td>   </td> <td>    <input type=submit value=Ready >   </td> </tr>";
+echo "</form>";
 
-    echo "</form>";
-
-    //mysqli_close($conn);
-    ?>
+//mysqli_close($conn);
+?>
 
     <button onclick="cansel()" >Cancel</button>
 
 </body>
 </html>
-        
 
-        
+
+
 <?php
-         function geefAnntalVanItemInOrderLines($pOrder, $pItem) {
-        $aantalItemsInOrder = 0;
-        $conn = connectToDb();
-        $sql = sprintf("SELECT * FROM  `orderLines`  where  `order` = %d   and `item` = %d ", $pOrder, $pItem);
-        echo $sql;
-        $result = mysqli_query($conn, $sql);
-        if (count($result) == 0) {
-            return 0;
-        } else {
-            while ($row = mysqli_fetch_array($result)) {
-                $aantalItemsInOrder += $row['amount'];
-            }
+
+function geefAnntalVanItemInOrderLines($pOrder, $pItem) {
+    $aantalItemsInOrder = 0;
+    $conn = connectToDb();
+    $sql = sprintf("SELECT * FROM  `orderLines`  where  `order` = %d   and `item` = %d ", $pOrder, $pItem);
+    echo $sql;
+    $result = mysqli_query($conn, $sql);
+    if (count($result) == 0) {
+        return 0;
+    } else {
+        while ($row = mysqli_fetch_array($result)) {
+            $aantalItemsInOrder += $row['amount'];
         }
-        return $aantalItemsInOrder;
     }
+    return $aantalItemsInOrder;
+}
 ?>
 
-        
-        
+
--- insertOrderLinesDB.php
+++ insertOrderLinesDB.php
@@ -7,88 +7,104 @@ require_once './model.php';
 echo "<br>";
 $teverwerkenAantal = count($_REQUEST);
 $teverwerkenAantal = $teverwerkenAantal / 2;
-
+$errorText = "Changes are implemented";
 $conn = connectToDb();
-    if ($conn->connect_error) {
-        printf("Errormessage: %s\n", $conn->error);
-    }
+//var_dump($conn);
+if ($conn->connect_error) {
+    verwerkError(sprintf("Errormessage: %s\n", $conn->error));
+}
 
 for ($i = 1; $i <= $teverwerkenAantal; $i++) {
-    $naamAantal         = "aantal" . $i;
-    $naamHiddenAantal   = "hiddenaantal" . $i;
-    $nieuwAantal        = $_REQUEST[$naamAantal];
-    $verschil           = $_REQUEST[$naamHiddenAantal] - $_REQUEST[$naamAantal];
+    $naamAantal = "aantal" . $i;
+    $naamHiddenAantal = "hiddenaantal" . $i;
+    $nieuwAantal = $_REQUEST[$naamAantal];
+    $vorigAantal = $_REQUEST[$naamHiddenAantal];
+    $verschil = $vorigAantal - $nieuwAantal;
     if ($verschil != 0) {
-        echo "Er is een verschil van: " .$verschil."<br>";
-        if ($_REQUEST[$naamHiddenAantal] == 0) {
-            insertInOrderLine($i, $order, $nieuwAantal,$conn);
-        } else {
-            updateOrderline($i, $order,$nieuwAantal,$conn);
+        echo "Er is een verschil van: " . $verschil . "op regel $i <br>";
+        if ($nieuwAantal != 0 && $vorigAantal == 0) {
+            insertInOrderLine($i, $order, $nieuwAantal);
         }
-        if ($_REQUEST[$naamAantal] == 0) {
-            deleteInOrderLine($i, $order, $nieuwAantal,$conn);
+
+        if ($nieuwAantal != 0 && $vorigAantal != 0) {
+            updateOrderline($i, $order, $nieuwAantal);
+        }
+
+        if ($nieuwAantal == 0 && $vorigAantal != 0) {
+            deleteOrderLine($i, $order, $nieuwAantal);
         }
     }
-    
-    
-//    header("Location: insertOrderLines.php");
+}
+$conn->close();
+header("Location: insertOrderLines.php?errorText=$errorText ");
 
-//    echo $_REQUEST[$naamAantal];
-//    echo alsAantalVerschilt($order, $_REQUEST[$naamAantal], $i);
+function deleteOrderLine($pItemTeller, $pOrder) {
+    echo $pItemTeller;
+
+    $zoekItem = converteerRijNummer2Item($pItemTeller);
+    echo $zoekItem;
+    $sql = sprintf("DELETE FROM `orderlines` WHERE `orderlines`.`order` = %d AND `orderlines`.`item` = %d", $pOrder, $zoekItem);
+    echo $sql;
+    echo "<br>";
+    $conn = connectToDb();
+    if (!$conn->query($sql)) {
+        verwerkError(sprintf("Errormessage: %s\n", $conn->error));
+    }
+    $conn->close();
 }
 
-function updateOrderline($pItemTeller, $pOrder, $pAmount,$conn) {
-//UPDATE `orderlines` SET `amount` = '55' WHERE `orderlines`.`order` = 7 AND `orderlines`.`item` = 1015;
-    $sql = sprintf("UPDATE `orderlines` SET `amount` = %d WHERE  `orderlines`.`order` = %d AND `orderlines`.`item` =  %d",$pAmount ,$pOrder, converteerRijNummer2Item($pItemTeller,$conn) );
+function updateOrderline($pItemTeller, $pOrder, $pAmoun) {
+    $sql = sprintf("UPDATE `orderlines` SET `amount` = %d WHERE  `orderlines`.`order` = %d AND `orderlines`.`item` =  %d", $pAmount, $pOrder, converteerRijNummer2Item($pItemTeller));
     echo $sql;
-
+    echo "<br>";
+    $conn = connectToDb();
     if (!$conn->query($sql)) {
-        printf("Errormessage: %s\n", $conn->error);
+        verwerkError(sprintf("Errormessage: %s\n", $conn->error));
     }
 }
 
-function insertInOrderLine($pItemTeller, $pOrder, $pAmount,$conn) {
-
-    $sql = sprintf("INSERT INTO `orderLines` ( `order`,  `item`, `amount`) VALUES ( %d, %d , %d)", $pOrder, converteerRijNummer2Item($pItemTeller,$conn), $pAmount);
+function insertInOrderLine($pItemTeller, $pOrder, $pAmount) {
+    $sql = sprintf("INSERT INTO `orderLines` ( `order`,  `item`, `amount`) VALUES ( %d, %d , %d)", $pOrder, converteerRijNummer2Item($pItemTeller, $conn), $pAmount);
     echo $sql;
-
+    echo "<br>";
+    $conn = connectToDb();
     if (!$conn->query($sql)) {
-        printf("Errormessage: %s\n", $conn->error);
+        verwerkError(sprintf("Errormessage: %s\n", $conn->error));
     }
 }
 
-function converteerRijNummer2Item($pItemTeller,$conn) {
+function converteerRijNummer2Item($pItemTeller) {
     // zoek item mbv line nr
     $localItem = 0;
-
-        $pItemTeller--;
-        $sql = "SELECT * FROM `item` WHERE 1 LIMIT 1 offset " . $pItemTeller;
-        echo $sql;
-        $resultSet = $conn->query($sql);
-        if (count($resultSet) != 0) {
-            $row = $resultSet->fetch_assoc();
-            var_dump($row);
-            $localItem = $row['item'];
-        }
-
+    $pItemTeller--;
+//    $sql = "SELECT * FROM `item` WHERE 1 LIMIT 1 offset " . $pItemTeller;
+    $sql = "SELECT * FROM `item` WHERE 1 LIMIT 1 offset " . $pItemTeller;
+    echo $sql;
+    $conn = connectToDb();
+    $resultSet = $conn->query($sql);
+    if (count($resultSet) != 0) {
+        $row = $resultSet->fetch_assoc();
+//        var_dump($row);
+        $localItem = $row['item'];
+    }
     return $localItem;
 }
 
+function verwerkError($pTxt) {
+    global $errorText;
+    $errorText = $pTxt;
+}
 
-
-
-function alsAantalVerschilt($aantal, $itemTeller,$conn) {
-//    
-//    $aantal = 0;
-//    $conn = connectToDb();
-//    $sql = sprintf("SELECT * FROM  `orderLines`  where  `order` = %d   and `item` = %d ", $pOrder, $pItem);
-//    echo $sql;
-//    $result = mysqli_query($conn, $sql);
-//     while ($row = mysqli_fetch_array($result)) {
-//         echo $row['item'];
-//         $aantal = $row['amount'];
-//     } 
-    echo $$aantal;
+function bestaatOrderLine($pZoekItem, $pOrder) {
+    $sql = sprintf("SELECT * FROM `orderlines`   WHERE `orderlines`.`order` = %d AND `orderlines`.`item` = %d", $pOrder, $zoekItem);
+    echo "<br>".$sql."<br>";
+    $conn = connectToDb();
+    $resultSet = $conn->query($sql);
+    if (count($resultSet) != 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 ?>
